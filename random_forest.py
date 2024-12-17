33a44bb49be8390070e6d03076cd06e273422ba3
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-12-17 13:47:44 +0100

    Add helper class to compare results

--- random_forest.py
+++ random_forest.py
@@ -2,20 +2,20 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from statistics_helper import StatisticsHelper
 
 # Load the data
 # Assuming `full_employee_data` is already loaded as a DataFrame
 full_employee_data: pd.DataFrame = pd.read_csv('full_employee_data_cleaned.csv')
 
 # Prepare the data
-target_column = 'Attrition_Yes'
+target_column = 'Attrition'
 # Replace 'target_column' with the actual name of the target column
-X = full_employee_data.drop([target_column, 'Attrition_No'], axis=1)
+X = full_employee_data.drop(target_column, axis=1)
 y = full_employee_data[target_column]
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
 
 # Train the Random Forest model
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -24,8 +24,9 @@ rf_model.fit(X_train, y_train)
 # Make predictions
 y_pred = rf_model.predict(X_test)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}')
-print('Classification Report:')
-print(classification_report(y_test, y_pred))
+stats_helper = StatisticsHelper(X, y, rf_model, y_test, y_pred)
+stats_helper.show_accuracy()
+stats_helper.show_classification_report()
+stats_helper.show_confusion_matrix()
+stats_helper.show_cross_val_score()
+stats_helper.show_roc_auc_score()
---
+++ statistics_helper.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score
+from sklearn.model_selection import cross_val_score
+
+
+class StatisticsHelper:
+    def __init__(self, X, y, model, y_test, _pred):
+        self.X = X
+        self.y = y
+        self.model = model
+        self.y_test = y_test
+        self.y_pred = _pred
+
+    def show_accuracy(self):
+        accuracy = accuracy_score(self.y_test, self.y_pred)
+        print(f'Accuracy: {accuracy}')
+
+    def show_roc_auc_score(self):
+        roc_auc = roc_auc_score(self.y_test, self.y_pred)
+        print(f'ROC AUC Score: {roc_auc}')
+
+    def show_classification_report(self):
+        print('Classification Report:')
+        print(classification_report(self.y_test, self.y_pred))
+
+    def show_confusion_matrix(self):
+        cm = confusion_matrix(self.y_test, self.y_pred)
+
+        plt.imshow(cm, cmap='Blues', interpolation='nearest')
+        plt.colorbar()
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.xticks([0, 1], ['No', 'Yes'])
+        plt.yticks([0, 1], ['No', 'Yes'])
+        plt.title('Confusion Matrix')
+        # Annotate the confusion matrix with the counts
+        for i in range(cm.shape[0]):
+            for j in range(cm.shape[1]):
+                plt.text(j, i, format(cm[i, j], 'd'), ha='center', va='center', color='black')
+
+        plt.show()
+
+    def show_cross_val_score(self):
+        scores = cross_val_score(self.model, self.X, self.y, cv=5)
+        print(f'Cross-validated scores: {scores}')
+        print(f'Mean accuracy: {scores.mean()}')
+        print(f'Standard deviation: {scores.std()}')
